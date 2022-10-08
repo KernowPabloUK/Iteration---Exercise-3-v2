fefbb1fc540aa25ed10c1a3a8698fb67a4a65f9a
KernowPabloUK <PaulMorganUK1@outlook.com>
2022-10-08 16:33:52 +0100

    Add project files.

---
+++ Iteration - Exercise 3 v2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Iteration___Exercise_3_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number & I will calculate the factorial for you!!!");
+           
+            int input = Convert.ToInt32(Console.ReadLine());
+          //  int output = input;
+           // int factorialOutput = 0;
+            int factorialResult = 1;
+
+                for (int userNumber = input; userNumber >= 1; userNumber --)
+                {
+                //factorialOutput = factorial * output; // unnecessary line
+
+                factorialResult = factorialResult * userNumber;
+
+                }
+
+            Console.WriteLine($"The Factorial is {input}! = {factorialResult} ");
+        }
+    }
+}
